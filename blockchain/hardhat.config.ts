c32c30dd5bb759e00157493cbe9c797407eb21bd
alexGarridoCC <alejandro.garrido@coincover.com>
2024-09-02 20:03:23 -0300

    feat: add fooundry plugin + uniswap v4 libs

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
+import "dotenv/config";
+
+const { PRIVATE_KEY, ALCHEMY_API_KEY, ETHERSCAN_API_KEY } =	process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.24",  
 };
 
 export default config;
