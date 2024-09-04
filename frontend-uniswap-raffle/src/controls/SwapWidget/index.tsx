8ad0ce1b28c3c90634b2faf2e167b80bf36bb454
Magofoco <francesco.stefani@outlook.com>
2024-09-04 22:05:16 +0200

    add widget not working

--- frontend-uniswap-raffle/src/controls/SwapWidget/index.tsx
+++ frontend-uniswap-raffle/src/controls/SwapWidget/index.tsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SwapWidget } from "@uniswap/widgets";
-import "@uniswap/widgets/fonts.css";
 import { Grid } from "../../components";
-import { ethers } from "ethers";
+import { Web3Provider } from "@ethersproject/providers";
+import { useAccount } from "wagmi";
+
+declare global {
+  interface Window {
+    Browser?: {
+      T: () => void;
+    };
+  }
+}
+
 const VITE_INFURA_API_KEY = import.meta.env.VITE_INFURA_API_KEY;
+const SEPOLIA_RPC_URL = `https://sepolia.infura.io/v3/${VITE_INFURA_API_KEY}`;
 
 export const SwapWidgetComponent: React.FC = () => {
-  const SEPOLIA_RPC_URL = `https://sepolia.infura.io/v3/${VITE_INFURA_API_KEY}`;
+  const [provider, setProvider] = useState<Web3Provider | undefined>();
+  const { connector } = useAccount();
 
-  const provider = new ethers.providers.JsonRpcProvider(SEPOLIA_RPC_URL);
+  useEffect(() => {
+    window.Browser = {
+      T: () => {},
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!connector) {
+      setProvider(undefined);
+      return;
+    }
+
+    const getProvider = async () => {
+      try {
+        const provider = await connector.getProvider();
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        setProvider(new Web3Provider(provider as any));
+      } catch (error) {
+        console.error("Failed to get provider:", error);
+        setProvider(undefined);
+      }
+    };
+
+    getProvider();
+  }, [connector]);
 
   const jsonRpcUrlMap = {
     11155111: SEPOLIA_RPC_URL,
@@ -16,7 +51,11 @@ export const SwapWidgetComponent: React.FC = () => {
 
   return (
     <Grid>
-      <SwapWidget provider={provider} jsonRpcUrlMap={jsonRpcUrlMap} />
+      <SwapWidget
+        tokenList={"https://ipfs.io/ipns/tokens.uniswap.org"}
+        provider={provider}
+        jsonRpcUrlMap={jsonRpcUrlMap}
+      />
     </Grid>
   );
 };
